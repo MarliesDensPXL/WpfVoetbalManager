a44206ad24d3f78136d1ac8de498d5a200c528b5
Marlies Dens <marlies.dens@pxl.be>
2026-05-15 14:57:02 +0200

    modellen

---
+++ VoetbalManager/Models/Footballer.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoetbalManager.Models
+{
+    public class Footballer
+    {
+		private string _firstName;
+
+		public string FirstName
+		{
+			get { return _firstName; }
+			set { _firstName = value; }
+		}
+
+		private string _lastName;
+
+		public string LastName
+		{
+			get { return _lastName; }
+			set { _lastName = value; }
+		}
+
+		private string _position;
+
+		public string Position
+		{
+			get { return _position; }
+			set
+			{
+				if (value == "goalkeeper" || value == "attacker" || value == "midfielder" || value == "defender")
+				{
+					_position = value;
+				}
+			}
+		}
+
+		private int _jerseyNumber;
+
+		public int JerseyNumber
+		{
+			get { return _jerseyNumber; }
+			set
+			{
+				if (value >= 0 && value <= 100)
+				{
+					_jerseyNumber = value;
+				}
+			}
+		}
+
+		private int _numberOfGoals;        
+
+        public int NumberOfGoals
+		{
+			get { return _numberOfGoals; }
+			set { _numberOfGoals = value; }
+		}
+
+        public Footballer(string firstName, string lastName, string position, int jerseyNumber, int numberOfGoals)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Position = position;
+            JerseyNumber = jerseyNumber;
+            NumberOfGoals = numberOfGoals;
+        }
+
+        public Footballer(string firstName, string lastName, int jerseyNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            JerseyNumber = jerseyNumber;
+			Position = "midfielder";
+			NumberOfGoals = 0;
+        }
+
+        public override string? ToString()
+        {
+			return $"{LastName} - {FirstName}";
+        }
+
+		public string Informatie()
+		{
+			return $"Voetballer {LastName} {FirstName} ({JerseyNumber}) \n" +
+				$"heeft dit seizoen {NumberOfGoals} doelen gescoord.\n" +
+				$"Positie: {Position}";
+		}
+
+	}
+}
---
+++ VoetbalManager/Models/Team.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoetbalManager.Models
+{
+    public class Team
+    {
+        private List<Footballer> _footballers;
+        
+        public List<Footballer> Footballers
+        {
+            get { return _footballers; }            
+        }
+
+
+        private string _teamName;        
+
+        public string TeamName
+		{
+			get { return _teamName; }
+			set { _teamName = value; }
+		}
+
+        private Footballer _captain;
+
+        public Footballer Captain
+        {
+            get { return _captain; }
+            set 
+            {
+                if (_footballers.Contains(value))
+                {
+                    _captain = value;
+                }
+            }
+        }
+
+        public Team(string teamName)
+        {
+            TeamName = teamName;
+        }
+
+        public void AddFootballer(Footballer footballer, bool isCaptain)
+        {
+            _footballers.Add(footballer);
+
+            if (isCaptain)
+            {
+                Captain = footballer;
+            }
+        }
+
+        public void RemoveFootballer(Footballer footballer)
+        {
+            _footballers.Remove(footballer);
+        }
+
+        public override string ToString()
+        {
+            return $"{TeamName}";
+        }
+
+        public double AverageNumberOfGoals()
+        {
+            double numberOfGoals = 0;
+            foreach (Footballer fb in _footballers)
+            {
+                numberOfGoals += fb.NumberOfGoals;
+            }
+
+           double averageNumberOfGoals = (numberOfGoals / _footballers.Count);
+
+            return averageNumberOfGoals;
+        }
+
+        public string TeamInformation()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{TeamName} heeft {_footballers.Count} spelers");
+            sb.AppendLine("----------------------");
+            foreach (Footballer fb in _footballers)
+            {
+                sb.AppendLine($"{fb.LastName} {fb.FirstName}");
+            }
+            sb.AppendLine("----------------------");                       
+            sb.AppendLine($"Gemiddeld aantal doelpunten: {AverageNumberOfGoals()}");
+
+            return sb.ToString();                  
+               
+        }
+    }
+}
